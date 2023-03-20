e4535c7463b552be691128d5a45ae1d385be62cb
xdre14 <kr.xandre@gmail.com>
2023-03-20 11:20:07 +0200

    added homework#4

--- home-work3.js
+++ home-work3.js
@@ -1,109 +1,109 @@
-// task 1
-const arr = [5, 3, 4, 5, 6, 7, 3];
-function duplicates(arr) {
-    let emptyArr = [];
-    for (let i = 0; i < arr.length; i++) {
-      if (emptyArr.indexOf(arr[i]) === -1) {
-        emptyArr.push(arr[i]);
-      }
-    }
-    return emptyArr;
-  }
-  const emptyArr = duplicates(arr);
-  console.log(emptyArr);
+// // task 1
+// const arr = [5, 3, 4, 5, 6, 7, 3];
+// function duplicates(arr) {
+//     let emptyArr = [];
+//     for (let i = 0; i < arr.length; i++) {
+//       if (emptyArr.indexOf(arr[i]) === -1) {
+//         emptyArr.push(arr[i]);
+//       }
+//     }
+//     return emptyArr;
+//   }
+//   const emptyArr = duplicates(arr);
+//   console.log(emptyArr);
   
 
-//   task 2
-function endStart(start, end) {
-    let myArr = [];
-    for ( let i = start; i <= end; i++){
-        arr.push(i);
-  }
-  return arr;
-}
-console.log(endStart (4, 12));
+// //   task 2
+// function endStart(start, end) {
+//     let myArr = [];
+//     for ( let i = start; i <= end; i++){
+//         arr.push(i);
+//   }
+//   return arr;
+// }
+// console.log(endStart (4, 12));
  
 
-// task 3
+// // task 3
 
-function printNumbers(a, b) {
-    for (let i = a; i <= b; i++) {
-      for (let c = 0; c < i - a + 1; c++) {
-        console.log(i);
-      }
-    }
-  }
-  console.log(printNumbers (4, 10));
-
-
-  // task 4
+// function printNumbers(a, b) {
+//     for (let i = a; i <= b; i++) {
+//       for (let c = 0; c < i - a + 1; c++) {
+//         console.log(i);
+//       }
+//     }
+//   }
+//   console.log(printNumbers (4, 10));
 
 
-  function randArray(k) {
-  const arr = [];
-  for (var i = 0; i < k; i++) {
-    arr.push(Math.floor(Math.random() * 500) + 1);
-  }
-  return arr;
-}
-randArray(5);
-
-// task 5
-let arr1 = [5, "Limit", 12, "a", "3", 99, 2, [2, 4, 3, "33", "a", "text"], "strong", "broun"];
-
-function arrType(arr1) {
-  let result = [];
-  let temp = {};
-
-  // Iterate through the array and group the elements by their type
-  for (let i = 0; i < arr1.length; i++) {
-    let type = typeof arr1[i];
-    if (!temp[type]) {
-      temp[type] = [];
-    }
-    temp[type].push(arr[i]);
-  }
-  for (var type in temp) {
-    result.push(temp[type]);
-  }
-
-  return result;
-}
-
-let arrNew = arrType(arr1);
-console.log(arrNew);
+//   // task 4
 
 
-// task 6
+//   function randArray(k) {
+//   const arr = [];
+//   for (var i = 0; i < k; i++) {
+//     arr.push(Math.floor(Math.random() * 500) + 1);
+//   }
+//   return arr;
+// }
+// randArray(5);
 
-function calc(a, b, op) {
-  switch (op) {
-    case 1:
-      return a - b;
-    case 2:
-      return a * b;
-    case 3:
-      return a / b;
-    default:
-      return a + b;
-  }
-}
+// // task 5
+// let arr1 = [5, "Limit", 12, "a", "3", 99, 2, [2, 4, 3, "33", "a", "text"], "strong", "broun"];
 
-// task 7
+// function arrType(arr1) {
+//   let result = [];
+//   let temp = {};
 
-function findUnique(arr) {
-  const count = {};
+//   // Iterate through the array and group the elements by their type
+//   for (let i = 0; i < arr1.length; i++) {
+//     let type = typeof arr1[i];
+//     if (!temp[type]) {
+//       temp[type] = [];
+//     }
+//     temp[type].push(arr[i]);
+//   }
+//   for (var type in temp) {
+//     result.push(temp[type]);
+//   }
 
-  for (let i = 0; i < arr.length; i++) {
-    if (count[arr[i]]) {
-      return false;
-    } else {
-      count[arr[i]] = 1;
-    }
-  }
-  return true;
-}
+//   return result;
+// }
 
-findUnique([ 2, 4, 5, 5, 8]);
-findUnique([ 2, 4, 3, 5, 8]);
+// let arrNew = arrType(arr1);
+// console.log(arrNew);
+
+
+// // task 6
+
+// function calc(a, b, op) {
+//   switch (op) {
+//     case 1:
+//       return a - b;
+//     case 2:
+//       return a * b;
+//     case 3:
+//       return a / b;
+//     default:
+//       return a + b;
+//   }
+// }
+
+// // task 7
+
+// function findUnique(arr) {
+//   const count = {};
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (count[arr[i]]) {
+//       return false;
+//     } else {
+//       count[arr[i]] = 1;
+//     }
+//   }
+//   return true;
+// }
+
+// findUnique([ 2, 4, 5, 5, 8]);
+// findUnique([ 2, 4, 3, 5, 8]);
 
---
+++ home-work4.js
@@ -0,0 +1,131 @@
+// task 1
+
+function sumSliceArray(arr, first, second) {
+        if (typeof first !== "number" || typeof second !== "number") {
+        throw new Error("Перші два аргументи мають бути числами!");
+        }        
+        if (first >= second) {
+        throw new Error();
+        } if (first >= arr.length || second >= arr.length) {
+      throw new Error();
+    } let sum = 0;
+    for (let i = first; i <= second; i++) {
+      sum += arr[i];
+    }
+    
+    return sum;
+  }
+ 
+  const arr = [1, 2, 3, 4, 5, 6];
+  try {
+    const result = sumSliceArray(arr, 2, 4);
+    console.log(result); 
+  } catch (error) {
+    console.error(error.message);
+  }
+
+//   task 2
+
+function checkAge() {
+  let name = prompt("Please enter your name");
+  let ageInput = prompt("Please enter your age");
+  let status = prompt("Please enter your status (admin, moderator, or user)");
+
+  try {
+    if (!name || !ageInput || !status) {
+      throw new Error("The field is empty! Please enter your age");
+    }
+
+    let age = parseInt(ageInput);
+    if (isNaN(age)) {
+      throw new TypeError("Age should be a number");
+    }
+
+    if (age < 18 || age > 70) {
+      throw new RangeError("You are not allowed to watch the movie");
+    }
+
+    if (status !== "admin" && status !== "moderator" && status !== "user") {
+      throw new EvalError("Invalid status");
+    }
+
+    alert(`Welcome ${name}! You can watch the movie.`);
+  } catch (error) {
+    alert(`Error: ${error.name}. ${error.message}`);
+  }
+}
+
+// task 3 
+function calcRectangleArea(width, height) {
+    if (typeof width !== 'number' || typeof height !== 'number') {
+      throw new TypeError('Both parameters must be numbers');
+    }  
+    return width * height;
+  }
+  try {
+    const width = parseFloat(prompt('Enter rectangle width:'));
+    const height = parseFloat(prompt('Enter rectangle height:'));
+  
+    const area = calcRectangleArea(width, height);
+    alert(`The area of the rectangle is ${area}`);
+  } catch (e) {
+    alert(`An error occurred: ${e.message}`);
+  }
+
+//   task 4 
+class MonthException {
+    constructor(message) {
+      this.name = 'MonthException';
+      this.message = message;
+    }
+  }
+  
+  function showMonthName(month) {
+    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+                        'July', 'August', 'September', 'October', 'November', 'December'];
+    if (month < 1 || month > 12) {
+      throw new MonthException('Not month number!');
+    }
+    return monthNames[month - 1];
+  }
+  
+  try {
+    console.log(showMonthName(5));
+    console.log(showMonthName(14));
+  } catch (e) {
+    if (e instanceof MonthException) {
+      console.log(e.name, e.message);
+    } else {
+      throw e;
+    }
+  }
+
+//   task 5
+
+class NegativeIdError extends Error {
+    constructor(message) {
+      super(message);
+      this.name = 'NegativeIdError';
+    }
+  }
+  
+  function showUser(id) {
+    if (id < 0) {
+      throw new NegativeIdError('ID must not be negative: ' + id);
+    }
+    return { id: id };
+  }
+  
+  function showUsers(ids) {
+    const users = [];
+    for (let i = 0; i < ids.length; i++) {
+      try {
+        users.push(showUser(ids[i]));
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+    return users;
+  }
+  
+  console.log(showUsers([7, -12, 44, 22]));
